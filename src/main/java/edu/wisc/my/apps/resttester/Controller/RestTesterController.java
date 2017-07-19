7b5a6ef124d56f56677761351a4937cc5ddf0ff1
Doug Reed <doug.reed@wisc.edu>
2017-07-19 10:58:29 -0500

    Further work

--- src/main/java/edu/wisc/my/apps/resttester/Controller/RestTesterController.java
+++ src/main/java/edu/wisc/my/apps/resttester/Controller/RestTesterController.java
@@ -11,6 +11,9 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 @Controller
 public class RestTesterController{
@@ -23,7 +26,41 @@ public class RestTesterController{
 
     @RequestMapping(value="/")
     public @ResponseBody void getJson(HttpServletRequest request, HttpServletResponse response){
- try {
+    StringBuilder result = new StringBuilder("");
+    try {
+          String linkLocation = env.getProperty("link");
+          logger.error(linkLocation);
+
+	//Get file from resources folder
+	ClassLoader classLoader = getClass().getClassLoader();
+	if(classLoader==null){
+        logger.error("No class loader");
+    }
+
+    if(classLoader.getResource(linkLocation)==null){
+        logger.error("Cannot get resource at " + linkLocation);
+    }
+
+    if(classLoader.getResource(linkLocation).getFile()==null){
+        logger.error("Screwy step 3");
+    }
+    File file = new File(classLoader.getResource(linkLocation).getFile());
+
+	try (Scanner scanner = new Scanner(file)) {
+
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			result.append(line).append("\n");
+		}
+
+		scanner.close();
+
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+          String links = result.toString();
+          
+          JSONObject obj = new JSONObject(links);
           JSONObject responseObj = new JSONObject();
           responseObj.put("status", "up");
           response.getWriter().write(responseObj.toString());
